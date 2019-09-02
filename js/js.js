fad7c1688f4c95c1e9f24960d8c62dd74aee4112
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-09-02 16:15:58 -0500

    retrieving screen value via JavaScript

--- js/js.js
+++ js/js.js
@@ -1,1 +1,10 @@
 console.log("JavaScript is running");
+
+var screen = document.getElementsByClassName("screen")[0];
+screen.addEventListener("input", screenChange);
+
+function screenChange(e)
+{
+	console.log("screen value: " + e.target.value);
+}
+
